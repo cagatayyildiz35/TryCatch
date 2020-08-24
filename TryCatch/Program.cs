53845b3421819dab9a628545123e1b1bad2357f2
Bilge Adam <bilge.adam@baogrenci.com>
2020-08-24 10:01:29 +0300

    Add project files.

---
+++ TryCatch/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Lütfen bir sayı giriniz");
+
+            //string data = Console.ReadLine();
+
+            //int x;
+
+            //bool result = Int32.TryParse(data, out x);
+            ////int x = Convert.ToInt32(data);
+
+            //if (result)
+            //{
+            //    int sonuc = x * x;
+
+            //    Console.WriteLine(sonuc);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Format hatası var!");
+            //}
+            //Console.ReadLine();
+
+
+            //Console.WriteLine("Lütfen bir sayı giriniz");
+
+            //string data = Console.ReadLine();
+            //int x = 0;
+            //try
+            //{
+            //    x = Convert.ToInt32(data);
+            //    int sonuc = x * x;
+            //    Console.WriteLine(sonuc);
+            //}
+            //catch (Exception ex)
+            //{
+            //    string error = ex.Message;
+            //    Console.WriteLine("Sistemde bir hata meydana geldi");
+            //}
+
+            //Console.ReadLine();
+
+
+
+            //Console.WriteLine("Lütfen bir sayı giriniz");
+            //int x = Convert.ToInt32(Console.ReadLine());
+
+            //if (false)
+            //{
+            //    Console.WriteLine("asdasd");
+            //}
+
+
+        }
+    }
+}
